018f73efcf0ee4b9cc9ae1e6ccc978c79130884b
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-11-20 12:23:30 +0000

    Remove comment

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,3 @@ fn main() {
     let instance = Instance::from_str(include_str!("../instances/instance.csv")).unwrap();
     println!("{:#?}", bnb::branch_and_bound(&instance));
 }
-
-// TODO
-// - Kind of operation (arrival or departure) is not taken into account when scheduling
