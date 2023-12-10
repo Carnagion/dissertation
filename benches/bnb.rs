c2661b398853b6bfbea214a5646b434445b57cd9
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-12-10 10:06:10 +0000

    Bench all instances

--- benches/bnb.rs
+++ benches/bnb.rs
@@ -8,7 +8,7 @@ fn main() {
     divan::main();
 }
 
-#[divan::bench(consts = [1, 2, 3, 4, 5, 6])]
+#[divan::bench(consts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])]
 fn branch_and_bound<const INSTANCE: usize>(bencher: Bencher) {
     bencher
         .with_inputs(|| {
