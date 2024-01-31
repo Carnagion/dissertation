c3ea923debcb0fc68901487958aec952817fb4e5
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-01-31 16:33:03 +0000

    Add hover indicators in visualiser output

--- irdis/irdis-vis/src/lib.rs
+++ irdis/irdis-vis/src/lib.rs
@@ -3,10 +3,7 @@ use std::time::Duration;
 use chrono::NaiveTime;
 
 use svg::{
-    node::{
-        element::{Group, Line, Rectangle, Title},
-        Text,
-    },
+    node::element::{Group, Line, Rectangle, Style},
     Document,
 };
 
@@ -16,9 +13,26 @@ use irdis_core::instance::{
     Instance,
 };
 
-const SCALE_X: usize = 9;
+const SCALE: usize = 20;
 
-const SCALE_Y: usize = 9;
+const FILL_BLACK: &str = "fill: #000000;";
+
+const HOLLOW_BLACK: &str = "stroke: #000000; fill-opacity: 0;";
+
+const FILL_GREY: &str = "fill: #cccccc;";
+
+const FILL_RED: &str = "fill: #c70039;";
+
+const FILL_YELLOW: &str = "fill: #ffd23f;";
+
+const HM: &str = "%H:%M";
+
+macro_rules! title {
+    ($($arg:tt)*) => {
+        ::svg::node::element::Title::new()
+            .add(::svg::node::Text::new(::std::format!($($arg)*)))
+    }
+}
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Visualiser {}
@@ -30,47 +44,35 @@ impl Visualiser {
 
     pub fn visualise(&self, schedule: &[Op], instance: &Instance) -> Option<Document> {
         let starting_time = starting_time(schedule, instance)?;
-
-        let doc = schedule
-            .iter()
-            .enumerate()
-            .fold(Document::new(), |doc, (idx, op)| {
-                let line_y = idx * SCALE_Y;
-                let line_height = SCALE_Y - 2;
-
-                let rect_y = (idx * SCALE_Y) + 1;
-                let rect_height = SCALE_Y - 4;
-
-                let constraints = &instance.rows()[op.aircraft_idx].constraints;
-
-                let group = match (&op.schedule, constraints) {
-                    (OpSchedule::Departure(dep), OpConstraints::Departure(constraints)) => self
-                        .visualise_departure(
-                            dep,
-                            constraints,
-                            line_y,
-                            line_height,
-                            rect_y,
-                            rect_height,
-                            starting_time,
-                        ),
-                    (OpSchedule::Arrival(arr), OpConstraints::Arrival(constraints)) => {
-                        self.visualise_arrival(arr, constraints, line_y, line_height, starting_time)
-                    },
-                    _ => unreachable!(),
-                };
-
-                doc.add(group)
-            });
-
         let ending_time = schedule.last()?.schedule.op_time();
 
-        let width = (ending_time - starting_time).num_minutes() as usize * SCALE_X;
-        let height = schedule.len() * SCALE_Y;
+        let width = width(ending_time, starting_time);
+        let height = schedule.len() * SCALE;
 
-        let doc = doc
-            .set("width", format!("{}px", width + 2))
-            .set("height", format!("{}px", height + 2));
+        let style = Style::new(
+            ".hide { fill-opacity: 0; stroke-opacity: 0; }
+            .hide:hover { fill-opacity: 1; stroke-opacity: 1; }",
+        )
+        .set("type", "text/css");
+
+        let doc = Document::new()
+            .set("width", format!("{}px", width + 4))
+            .set("height", format!("{}px", height + 2))
+            .add(style);
+
+        let doc = schedule.iter().enumerate().fold(doc, |doc, (idx, op)| {
+            let constraints = &instance.rows()[op.aircraft_idx].constraints;
+            let group = match (&op.schedule, constraints) {
+                (OpSchedule::Departure(dep), OpConstraints::Departure(constraints)) => {
+                    self.visualise_departure(dep, constraints, idx, starting_time)
+                },
+                (OpSchedule::Arrival(arr), OpConstraints::Arrival(constraints)) => {
+                    self.visualise_arrival(arr, constraints, idx, starting_time)
+                },
+                _ => unreachable!(),
+            };
+            doc.add(group)
+        });
 
         Some(doc)
     }
@@ -79,96 +81,197 @@ impl Visualiser {
         &self,
         dep: &DepartureSchedule,
         constraints: &DepartureConstraints,
-        line_y: usize,
-        line_height: usize,
-        rect_y: usize,
-        rect_height: usize,
+        idx: usize,
         starting_time: NaiveTime,
     ) -> Group {
-        let take_off_x = (dep.take_off_time - starting_time).num_minutes() as usize * SCALE_X;
-        let take_off_title = title(format!("Take-off at {}", dep.take_off_time.format("%H:%M")));
-        let take_off = line(take_off_x, line_y, line_height)
-            .set("style", "stroke: #000000")
-            .add(take_off_title);
+        let take_off = {
+            let x = width(dep.take_off_time, starting_time);
+            let y = idx * SCALE;
 
-        let lineup_width = minutes(constraints.lineup_dur);
-        let lineup_title = title(format!("Lineup on runway for {} minutes", lineup_width));
-        let lineup_width = lineup_width * SCALE_X;
-        let lineup = rect(take_off_x - lineup_width, rect_y, lineup_width, rect_height)
-            .set("style", "fill: #e76f51")
-            .add(lineup_title);
+            let title = title!("Departure at {}", dep.take_off_time.format(HM));
 
-        let post_de_ice_width = minutes(constraints.post_de_ice_dur);
-        let post_de_ice_title = title(format!("Taxi for {} minutes", post_de_ice_width));
-        let post_de_ice_width = post_de_ice_width * SCALE_X;
-        let post_de_ice = rect(
-            take_off_x - lineup_width - post_de_ice_width,
-            rect_y,
-            post_de_ice_width,
-            rect_height,
-        )
-        .set("style", "fill: #f4a261")
-        .add(post_de_ice_title);
+            let bar = line(x, y + 3, 14);
+            let square = rect(x - 2, y + 8, 4, 4).set("style", FILL_BLACK);
+            Group::new().add(bar).add(square).add(title)
+        };
 
-        let de_ice_width = minutes(constraints.de_ice_dur);
-        let de_ice_title = title(format!("De-ice for {} minutes", de_ice_width));
-        let de_ice_width = de_ice_width * SCALE_X;
-        let de_ice = rect(
-            take_off_x - lineup_width - post_de_ice_width - de_ice_width,
-            rect_y,
-            de_ice_width,
-            rect_height,
-        )
-        .set("style", "fill: #e9c46a")
-        .add(de_ice_title);
+        let de_ice = {
+            let x = width(dep.de_ice_time, starting_time);
+            let y = idx * SCALE;
 
-        let pre_de_ice_width = minutes(constraints.pre_de_ice_dur);
-        let pre_de_ice_title = title(format!("Taxi for {} minutes", pre_de_ice_width));
-        let pre_de_ice_width = pre_de_ice_width * SCALE_X;
-        let pre_de_ice = rect(
-            take_off_x - lineup_width - post_de_ice_width - de_ice_width - pre_de_ice_width,
-            rect_y,
-            pre_de_ice_width,
-            rect_height,
-        )
-        .set("style", "fill: #2a9d8f")
-        .add(pre_de_ice_title);
+            let title = title!("De-ice at {}", dep.de_ice_time.format(HM));
 
-        let pushback_width = minutes(constraints.pushback_dur);
-        let pushback_title = title(format!("Pushback for {} minutes", pushback_width));
-        let pushback_width = pushback_width * SCALE_X;
-        let pushback = rect(
-            take_off_x
-                - lineup_width
-                - post_de_ice_width
-                - de_ice_width
-                - pre_de_ice_width
-                - pushback_width,
-            rect_y,
-            pushback_width,
-            rect_height,
-        )
-        .set("style", "fill: #264653")
-        .add(pushback_title);
+            let bar = line(x, y + 3, 14);
+            let square = rect(x - 2, y + 8, 4, 4).set("style", FILL_BLACK);
+            Group::new().add(bar).add(square).add(title)
+        };
 
-        let earliest_x =
-            (constraints.earliest_time - starting_time).num_minutes() as usize * SCALE_X;
-        let earliest_title = title(format!(
-            "Earliest possible take-off at {}",
-            constraints.earliest_time.format("%H:%M"),
-        ));
-        let earliest = line(earliest_x, line_y, line_height)
-            .set("style", "stroke: #000000")
-            .set("stroke-dasharray", "1 1")
-            .add(earliest_title);
+        let earliest = {
+            let x = width(constraints.earliest_time, starting_time);
+            let y = idx * SCALE;
+
+            let title = title!(
+                "Earliest possible departure at {}",
+                constraints.earliest_time.format(HM),
+            );
+
+            let bar = dashed_line(x, y, SCALE);
+            let square = rect(x - 2, y + 8, 4, 4).set("style", HOLLOW_BLACK);
+
+            Group::new().add(bar).add(square).add(title)
+        };
+
+        let delay = {
+            let x = width(constraints.earliest_time, starting_time);
+            let y = (idx * SCALE) + 5;
+
+            let width = width(dep.take_off_time, constraints.earliest_time);
+
+            let title = title!("{}-minute delay", width / SCALE);
+
+            rect(x, y, width, 10)
+                .set("style", FILL_RED)
+                .set("class", "hide")
+                .add(title)
+        };
+
+        let pushback = {
+            let x = width(
+                dep.take_off_time
+                    - (constraints.lineup_dur
+                        + constraints.post_de_ice_dur
+                        + constraints.de_ice_dur
+                        + constraints.pre_de_ice_dur
+                        + constraints.pushback_dur),
+                starting_time,
+            );
+            let y = (idx * SCALE) + 5;
+
+            let width = minutes(constraints.pushback_dur);
+
+            let title = title!("{} minutes to pushback from gates", width);
+
+            let bar = rect(x, y, width * SCALE, 10).set("style", FILL_YELLOW);
+            let underline = rect(x, y + 10, width * SCALE, 2).set("style", FILL_BLACK);
+
+            Group::new()
+                .add(bar)
+                .add(underline)
+                .add(title)
+                .set("class", "hide")
+        };
+
+        let pre_de_ice = {
+            let x = width(
+                dep.take_off_time
+                    - (constraints.lineup_dur
+                        + constraints.post_de_ice_dur
+                        + constraints.de_ice_dur
+                        + constraints.pre_de_ice_dur),
+                starting_time,
+            );
+            let y = (idx * SCALE) + 5;
+
+            let width = minutes(constraints.pre_de_ice_dur);
+
+            let title = title!("{} minutes to taxi to de-icing station", width);
+
+            let bar = rect(x, y, width * SCALE, 10).set("style", FILL_YELLOW);
+            let underline = rect(x, y + 10, width * SCALE, 2).set("style", FILL_BLACK);
+
+            Group::new()
+                .add(bar)
+                .add(underline)
+                .add(title)
+                .set("class", "hide")
+        };
+
+        let de_ice_dur = {
+            let x = width(
+                dep.take_off_time
+                    - (constraints.lineup_dur
+                        + constraints.post_de_ice_dur
+                        + constraints.de_ice_dur),
+                starting_time,
+            );
+            let y = (idx * SCALE) + 5;
+
+            let width = minutes(constraints.de_ice_dur);
+
+            let title = title!("{} minutes to de-ice", width);
+
+            let bar = rect(x, y, width * SCALE, 10).set("style", FILL_YELLOW);
+            let underline = rect(x, y + 10, width * SCALE, 2).set("style", FILL_BLACK);
+
+            Group::new()
+                .add(bar)
+                .add(underline)
+                .add(title)
+                .set("class", "hide")
+        };
+
+        let post_de_ice = {
+            let x = width(
+                dep.take_off_time - (constraints.lineup_dur + constraints.post_de_ice_dur),
+                starting_time,
+            );
+            let y = (idx * SCALE) + 5;
+
+            let width = minutes(constraints.post_de_ice_dur);
+
+            let title = title!("{} minutes to taxi to runway", width);
+
+            let bar = rect(x, y, width * SCALE, 10).set("style", FILL_YELLOW);
+            let underline = rect(x, y + 10, width * SCALE, 2).set("style", FILL_BLACK);
+
+            Group::new()
+                .add(bar)
+                .add(underline)
+                .add(title)
+                .set("class", "hide")
+        };
+
+        let lineup = {
+            let x = width(dep.take_off_time - constraints.lineup_dur, starting_time);
+            let y = (idx * SCALE) + 5;
+
+            let width = minutes(constraints.lineup_dur);
+
+            let title = title!("{} minutes to lineup on runway", width);
+
+            let bar = rect(x, y, width * SCALE, 10).set("style", FILL_YELLOW);
+            let underline = rect(x, y + 10, width * SCALE, 2).set("style", FILL_BLACK);
+
+            Group::new()
+                .add(bar)
+                .add(underline)
+                .add(title)
+                .set("class", "hide")
+        };
+
+        let background = {
+            let start = dep.take_off_time
+                - (constraints.lineup_dur
+                    + constraints.post_de_ice_dur
+                    + constraints.de_ice_dur
+                    + constraints.pre_de_ice_dur
+                    + constraints.pushback_dur);
+            let x = width(start, starting_time);
+            let y = (idx * SCALE) + 5;
+            let width = width(dep.take_off_time, start);
+            rect(x, y, width, 10).set("style", FILL_GREY)
+        };
 
         Group::new()
+            .add(background)
             .add(pushback)
             .add(pre_de_ice)
-            .add(de_ice)
+            .add(de_ice_dur)
             .add(post_de_ice)
             .add(lineup)
+            .add(delay)
             .add(earliest)
+            .add(de_ice)
             .add(take_off)
     }
 
@@ -176,34 +279,61 @@ impl Visualiser {
         &self,
         arr: &ArrivalSchedule,
         constraints: &ArrivalConstraints,
-        line_y: usize,
-        line_height: usize,
+        idx: usize,
         starting_time: NaiveTime,
     ) -> Group {
-        let landing_x = (arr.landing_time - starting_time).num_minutes() as usize * SCALE_X;
-        let landing_title = title(format!("Land at {}", arr.landing_time.format("%H:%M")));
-        let landing = line(landing_x, line_y, line_height)
-            .set("style", "stroke: #000000")
-            .add(landing_title);
+        let landing = {
+            let x = width(arr.landing_time, starting_time);
+            let y = idx * SCALE;
 
-        let earliest_x =
-            (constraints.earliest_time - starting_time).num_minutes() as usize * SCALE_X;
-        let earliest_title = title(format!(
-            "Earliest possible landing at {}",
-            constraints.earliest_time.format("%H:%M"),
-        ));
-        let earliest = line(earliest_x, line_y, line_height)
-            .set("style", "stroke: #000000")
-            .set("stroke-dasharray", "1 1")
-            .add(earliest_title);
+            let title = title!("Arrival at {}", arr.landing_time.format(HM));
 
-        Group::new().add(earliest).add(landing)
-    }
-}
+            let bar = line(x, y + 3, 14);
+            let square = rect(x - 2, y + 8, 4, 4).set("style", FILL_BLACK);
+            Group::new().add(bar).add(square).add(title)
+        };
 
-impl Default for Visualiser {
-    fn default() -> Self {
-        Self::new()
+        let earliest = {
+            let x = width(constraints.earliest_time, starting_time);
+            let y = idx * SCALE;
+
+            let title = title!(
+                "Earliest possible arrival at {}",
+                constraints.earliest_time.format(HM),
+            );
+
+            let bar = dashed_line(x, y, SCALE);
+            let square = rect(x - 2, y + 8, 4, 4).set("style", HOLLOW_BLACK);
+
+            Group::new().add(bar).add(square).add(title)
+        };
+
+        let delay = {
+            let x = width(constraints.earliest_time, starting_time);
+            let y = (idx * SCALE) + 5;
+
+            let width = width(arr.landing_time, constraints.earliest_time);
+
+            let title = title!("{}-minute delay", width / SCALE);
+
+            rect(x, y, width, 10)
+                .set("style", FILL_RED)
+                .set("class", "hide")
+                .add(title)
+        };
+
+        let background = {
+            let x = width(constraints.earliest_time, starting_time);
+            let y = (idx * SCALE) + 5;
+            let width = width(arr.landing_time, constraints.earliest_time);
+            rect(x, y, width, 10).set("style", FILL_GREY)
+        };
+
+        Group::new()
+            .add(background)
+            .add(delay)
+            .add(earliest)
+            .add(landing)
     }
 }
 
@@ -226,12 +356,21 @@ fn starting_time(schedule: &[Op], instance: &Instance) -> Option<NaiveTime> {
     Some(starting_time)
 }
 
+fn width(time: NaiveTime, from: NaiveTime) -> usize {
+    (time - from).num_minutes() as usize * SCALE
+}
+
 fn line(x: usize, y: usize, height: usize) -> Line {
     Line::new()
         .set("x1", x)
         .set("x2", x)
         .set("y1", y)
         .set("y2", y + height)
+        .set("style", "stroke: #000000;")
+}
+
+fn dashed_line(x: usize, y: usize, height: usize) -> Line {
+    line(x, y, height).set("stroke-dasharray", "2 1")
 }
 
 fn rect(x: usize, y: usize, width: usize, height: usize) -> Rectangle {
@@ -240,13 +379,6 @@ fn rect(x: usize, y: usize, width: usize, height: usize) -> Rectangle {
         .set("y", y)
         .set("width", width)
         .set("height", height)
-}
-
-fn title<T>(title: T) -> Title
-where
-    T: Into<String>,
-{
-    Title::new().add(Text::new(title))
 }
 
 fn minutes(dur: Duration) -> usize {
