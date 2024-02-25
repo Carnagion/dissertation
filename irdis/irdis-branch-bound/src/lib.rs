75c7b9d2be6529465261dc1a324e006e27b3a0c4
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-02-25 19:12:40 +0000

    Optimise branch-and-bound impl with more pruning

--- irdis/irdis-branch-bound/src/lib.rs
+++ irdis/irdis-branch-bound/src/lib.rs
@@ -1,6 +1,4 @@
-use std::{num::NonZeroUsize, ops::Range, time::Duration};
-
-use chrono::NaiveTime;
+use std::{num::NonZeroUsize, ops::Range};
 
 use irdis_instance::{
     flight::{Arrival, Departure, Flight},
@@ -37,219 +35,277 @@ impl Default for BranchBound {
 
 impl Solve for BranchBound {
     fn solve(&self, instance: &Instance) -> Vec<Schedule> {
-        let mut sep_identical_sets = separation_identical_sets(instance);
+        let mut sep_sets = separation_identical_sets(instance);
 
         let horizon = match self.horizon {
             None => 0..instance.flights().len(),
             Some(horizon) => 0..usize::from(horizon),
         };
 
-        let state = State {
-            last_set_idxs: vec![0; sep_identical_sets.len()],
-            current_schedule: Vec::new(),
-            best_schedule: Vec::new(),
-            current_bound: 0,
-            lowest_bound: u64::MAX,
+        let mut state = State {
+            next_in_sep_sets: vec![0; sep_sets.len()],
+            current_solution: Vec::with_capacity(instance.flights().len()),
+            best_solution: Vec::new(),
+            bounds: Bounds::default(),
             horizon,
         };
 
-        state.explore(instance, &mut sep_identical_sets)
+        state.branch_sep_sets(instance, &sep_sets);
+
+        while state.horizon.end < instance.flights().len() {
+            state.next_in_sep_sets.fill(0);
+
+            state.best_solution.drain(1..);
+            let sched = state.best_solution.pop().unwrap();
+
+            sep_sets
+                .iter_mut()
+                .for_each(|set| set.retain(|&flight_idx| flight_idx != sched.flight_index()));
+
+            state.current_solution.push(sched);
+
+            state.bounds = Bounds::default();
+
+            state.horizon.start += 1;
+            state.horizon.end += 1;
+
+            state.branch_sep_sets(instance, &sep_sets);
+        }
+
+        state.current_solution.extend(state.best_solution);
+        state.current_solution
     }
 }
 
-macro_rules! iter_minutes {
-    ($range:expr, |$elem:pat_param| $body:block) => {
-        {
-            let minute = Duration::from_secs(60);
-            let mut range = $range;
-            while range.start <= range.end {
-                let $elem = range.start;
+struct Bounds {
+    current: u64,
+    lowest: u64,
+}
 
-                $body
-
-                range.start += minute;
-            }
+impl Default for Bounds {
+    fn default() -> Self {
+        Self {
+            current: 0,
+            lowest: u64::MAX,
         }
-    };
+    }
 }
 
 struct State {
-    last_set_idxs: Vec<usize>,
-    current_schedule: Vec<Schedule>,
-    best_schedule: Vec<Schedule>,
-    current_bound: u64,
-    lowest_bound: u64,
+    next_in_sep_sets: Vec<usize>,
+    current_solution: Vec<Schedule>,
+    best_solution: Vec<Schedule>,
+    bounds: Bounds,
     horizon: Range<usize>,
 }
 
 impl State {
-    fn explore(
-        mut self,
-        instance: &Instance,
-        sep_identical_sets: &mut [Vec<usize>],
-    ) -> Vec<Schedule> {
-        self.explore_current(instance, sep_identical_sets);
-
-        while self.horizon.end < instance.flights().len() {
-            self.last_set_idxs.fill(0);
-
-            self.best_schedule.drain(1..);
-            let sched = self.best_schedule.pop().unwrap();
-
-            sep_identical_sets
-                .iter_mut()
-                .for_each(|set| set.retain(|&flight_idx| flight_idx != sched.flight_index()));
-
-            self.current_schedule.push(sched);
-
-            self.horizon.start += 1;
-            self.horizon.end += 1;
-
-            self.explore_current(instance, sep_identical_sets);
-        }
-
-        self.current_schedule.extend(self.best_schedule);
-        self.current_schedule
-    }
-
-    fn explore_current(&mut self, instance: &Instance, sep_identical_sets: &[Vec<usize>]) {
-        if self.current_schedule.len() == self.horizon.end {
-            self.update_best();
+    fn branch_sep_sets(&mut self, instance: &Instance, sep_sets: &[Vec<usize>]) {
+        if self.current_solution.len() == self.horizon.end {
+            self.update_best_solution();
         } else {
-            for (set_idx, sep_identical_set) in sep_identical_sets.iter().enumerate() {
-                let last_set_idx = self.last_set_idxs[set_idx];
-                let Some(&flight_idx) = sep_identical_set.get(last_set_idx) else {
+            for (set_idx, sep_set) in sep_sets.iter().enumerate() {
+                let next_idx = self.next_in_sep_sets[set_idx];
+                let Some(&flight_idx) = sep_set.get(next_idx) else {
                     continue;
                 };
-
-                let flight = &instance.flights()[flight_idx];
-                match flight {
-                    Flight::Arr(arrival) => self.explore_arrival(
-                        arrival,
-                        flight_idx,
-                        instance,
-                        sep_identical_sets,
-                        set_idx,
-                    ),
-                    Flight::Dep(departure) => self.explore_departure(
-                        departure,
-                        flight_idx,
-                        instance,
-                        sep_identical_sets,
-                        set_idx,
-                    ),
-                }
+                self.branch_flight(flight_idx, set_idx, instance, sep_sets);
             }
         }
     }
 
-    fn update_best(&mut self) {
-        if self.current_bound < self.lowest_bound {
-            self.lowest_bound = self.current_bound;
-            self.best_schedule = self.current_schedule.clone();
+    fn branch_flight(
+        &mut self,
+        flight_idx: usize,
+        set_idx: usize,
+        instance: &Instance,
+        sep_sets: &[Vec<usize>],
+    ) {
+        let flight = &instance.flights()[flight_idx];
+        match flight {
+            Flight::Arr(arr) => {
+                for sched in possible_arrs(arr, flight_idx, &self.current_solution, instance) {
+                    let landing_cost = landing_cost(&sched, arr);
+                    if self.bounds.current + landing_cost > self.bounds.lowest {
+                        continue;
+                    }
+                    self.branch_next(sched, landing_cost, set_idx, instance, sep_sets);
+                }
+            },
+            Flight::Dep(dep) => {
+                for sched in possible_deps(dep, flight_idx, &self.current_solution, instance) {
+                    let holdover_cost = holdover_cost(&sched, dep);
+                    if self.bounds.current + holdover_cost > self.bounds.lowest {
+                        continue;
+                    }
+                    self.branch_next(sched, holdover_cost, set_idx, instance, sep_sets);
+                }
+            },
         }
     }
 
-    fn explore_next(
+    fn branch_next<S>(
         &mut self,
-        sched: Schedule,
-        sched_cost: u64,
-        instance: &Instance,
-        sep_identical_sets: &[Vec<usize>],
+        sched: S,
+        cost: u64,
         set_idx: usize,
-    ) {
-        self.current_schedule.push(sched);
-        self.current_bound += sched_cost;
-        self.last_set_idxs[set_idx] += 1;
+        instance: &Instance,
+        sep_sets: &[Vec<usize>],
+    ) where
+        S: Into<Schedule>,
+    {
+        let sched = sched.into();
 
-        self.explore_current(instance, sep_identical_sets);
+        self.current_solution.push(sched);
+        self.bounds.current += cost;
+        self.next_in_sep_sets[set_idx] += 1;
 
-        self.current_schedule.pop();
-        self.current_bound -= sched_cost;
-        self.last_set_idxs[set_idx] -= 1;
+        self.branch_sep_sets(instance, sep_sets);
+
+        self.current_solution.pop();
+        self.bounds.current -= cost;
+        self.next_in_sep_sets[set_idx] -= 1;
     }
 
-    fn explore_arrival(
-        &mut self,
-        arrival: &Arrival,
-        arrival_idx: usize,
-        instance: &Instance,
-        sep_identical_sets: &[Vec<usize>],
-        set_idx: usize,
-    ) {
-        let prev_sched = self.current_schedule.last();
-        let landing_times = possible_landing_times(arrival, arrival_idx, prev_sched, instance);
-
-        iter_minutes!(landing_times, |landing| {
-            let sched = ArrivalSchedule {
-                flight_idx: arrival_idx,
-                landing,
-            };
-
-            let landing_cost = landing_cost(landing, arrival);
-            if self.current_bound + landing_cost > self.lowest_bound {
-                continue;
-            }
-
-            self.explore_next(
-                sched.into(),
-                landing_cost,
-                instance,
-                sep_identical_sets,
-                set_idx,
-            );
-        });
+    fn update_best_solution(&mut self) {
+        if self.bounds.current < self.bounds.lowest {
+            self.bounds.lowest = self.bounds.current;
+            self.best_solution = self.current_solution.clone();
+        }
     }
+}
 
-    fn explore_departure(
-        &mut self,
-        departure: &Departure,
-        departure_idx: usize,
-        instance: &Instance,
-        sep_identical_sets: &[Vec<usize>],
-        set_idx: usize,
-    ) {
-        let prev_sched = self.current_schedule.last();
-        let prev_dep_sched = self
-            .current_schedule
-            .iter()
-            .rev()
-            .find_map(Schedule::as_departure)
-            .cloned();
+// NOTE: This is a workaround the fact that `Range<T>` currently only impls `Iterator` if
+//       `T` impls `Step`. Of course, `Step` is still an unstable trait, so `chrono::NaiveTime`
+//       does not impl it, and thus it isn't possible to iterate over a `Range<NaiveTime>`.
+macro_rules! iter_minutes {
+    ($from:expr, $to:expr) => {{
+        let (from, to) = ($from, $to);
+        let diff = (from - to).num_minutes().unsigned_abs();
+        (0..=diff).map(move |minute| from + ::std::time::Duration::from_secs(minute * 60))
+    }};
+}
 
-        let takeoff_times = possible_takeoff_times(departure, departure_idx, prev_sched, instance);
+fn possible_arrs(
+    arr: &Arrival,
+    arr_idx: usize,
+    current_solution: &[Schedule],
+    instance: &Instance,
+) -> impl Iterator<Item = ArrivalSchedule> {
+    let prev_sched = current_solution.last();
 
-        iter_minutes!(takeoff_times, |takeoff| {
-            let takeoff_cost = takeoff_cost(takeoff, departure);
-            if self.current_bound + takeoff_cost > self.lowest_bound {
-                continue;
-            }
+    let (earliest_landing, latest_landing) = match prev_sched {
+        None => (arr.window.earliest(), arr.window.latest()),
+        Some(prev_sched) => {
+            let sep = instance.separations()[(prev_sched.flight_index(), arr_idx)];
+            let earliest_landing = arr.window.earliest().max(prev_sched.flight_time() + sep);
+            (earliest_landing, arr.window.latest())
+        },
+    };
 
-            let deice_times =
-                possible_deice_times(departure, takeoff, prev_dep_sched.as_ref(), instance);
+    iter_minutes!(earliest_landing, latest_landing).map(move |landing| ArrivalSchedule {
+        flight_idx: arr_idx,
+        landing,
+    })
+}
 
-            iter_minutes!(deice_times, |deice| {
-                let sched = DepartureSchedule {
-                    flight_idx: departure_idx,
-                    deice,
-                    takeoff,
-                };
+// NOTE: Since there is no penalty for taking off anywhere inside a departure's assigned
+//       CTOT slot, we try to take off as early as we possibly can.
+//
+//       First, the earliest possible take-off time is calculated based on the earliest
+//       allowed time in the departure's CTOT slot and the take-off or landing time of the
+//       previous flight plus separation, if any.
+//
+//       The departure's earliest and latest de-icing times can be calculated based on this.
+//
+//       If this is the first departure in the sequence, its earliest de-icing time is its
+//       earliest take-off time minus the maximum holdover duration minus its de-icing duration.
+//       Its latest de-icing time is its earliest take-off time minus its taxi and lineup durations
+//       minus its de-icing duration.
+//
+//       If there is a departure before it, its earliest and latest de-icing times are calculated
+//       in a similar manner as above, except are also compared with the time that the previous
+//       departure finishes de-icing, and the maximum of the values is taken.
+//
+//       On one hand, we want to de-ice as soon as possible to avoid any gaps in the de-icing queue,
+//       as these have a significantly large knock-on effect on the following departures. On the other
+//       hand, this means that delays are absorbed at the runway rather than at the gates, wasting fuel.
+//       The ideal de-icing time lies somewhere in this range, and depends on the delay and slack costs.
+//
+//       Finally, for each de-ice time within the range, we can calculate the corresponding take-off
+//       time as the maximum of the earliest takeoff time (calculated previously) and the time it
+//       takes to finish de-icing and get to the runway.
+//
+//       Since the earliest possible take-off time will always be greater than or equal to the CTOT
+//       window's earliest allowed time, and the actual take-off time will always be greater than
+//       or equal to the earliest take-off time, we know for sure that the take-off time will be
+//       after the CTOT window's earliest allowed time. However, it may still be after its latest
+//       allowed time, in which case the current branch becomes infeasible and can be ignored.
+fn possible_deps<'a>(
+    dep: &'a Departure,
+    dep_idx: usize,
+    current_solution: &[Schedule],
+    instance: &Instance,
+) -> impl Iterator<Item = DepartureSchedule> + 'a {
+    let prev_sched = current_solution.last();
+    let prev_dep_sched = current_solution
+        .iter()
+        .rev()
+        .find_map(Schedule::as_departure)
+        .cloned();
 
-                let slack_cost = slack_cost(deice, takeoff, departure);
-                if self.current_bound + takeoff_cost + slack_cost > self.lowest_bound {
-                    continue;
-                }
+    let earliest_takeoff = match prev_sched {
+        None => dep.ctot.earliest(),
+        Some(prev_sched) => {
+            let sep = instance.separations()[(prev_sched.flight_index(), dep_idx)];
+            dep.ctot.earliest().max(prev_sched.flight_time() + sep)
+        },
+    };
 
-                self.explore_next(
-                    sched.into(),
-                    takeoff_cost + slack_cost,
-                    instance,
-                    sep_identical_sets,
-                    set_idx,
-                );
-            });
-        });
-    }
+    let (earliest_deice, latest_deice) = match prev_dep_sched {
+        None => {
+            let earliest_deice = earliest_takeoff - instance.max_holdover_dur - dep.deice_dur;
+            let latest_deice = earliest_takeoff - dep.lineup_dur - dep.taxi_out_dur - dep.deice_dur;
+            (earliest_deice, latest_deice)
+        },
+        Some(prev_dep_sched) => {
+            let prev_dep = instance.flights()[prev_dep_sched.flight_idx]
+                .as_departure()
+                .unwrap();
+
+            let earliest_deice = (earliest_takeoff - instance.max_holdover_dur - dep.deice_dur)
+                .max(prev_dep_sched.deice + prev_dep.deice_dur);
+            let latest_deice =
+                (earliest_takeoff - dep.lineup_dur - dep.taxi_out_dur - dep.deice_dur)
+                    .max(prev_dep_sched.deice + prev_dep.deice_dur);
+
+            (earliest_deice, latest_deice)
+        },
+    };
+
+    iter_minutes!(earliest_deice, latest_deice).filter_map(move |deice| {
+        let takeoff =
+            earliest_takeoff.max(deice + dep.deice_dur + dep.taxi_out_dur + dep.lineup_dur);
+
+        (dep.ctot.earliest()..=dep.ctot.latest())
+            .contains(&takeoff)
+            .then_some(DepartureSchedule {
+                flight_idx: dep_idx,
+                deice,
+                takeoff,
+            })
+    })
+}
+
+fn landing_cost(sched: &ArrivalSchedule, arr: &Arrival) -> u64 {
+    let delay = sched.landing - arr.window.target;
+    delay.num_minutes().unsigned_abs().pow(2)
+}
+
+fn holdover_cost(sched: &DepartureSchedule, dep: &Departure) -> u64 {
+    let tightest_deice = sched.takeoff - dep.lineup_dur - dep.taxi_out_dur - dep.deice_dur;
+    let slack = tightest_deice - sched.deice;
+    slack.num_minutes().unsigned_abs().pow(2)
 }
 
 fn separation_identical_sets(instance: &Instance) -> Vec<Vec<usize>> {
@@ -284,90 +340,4 @@ fn separation_identical_sets(instance: &Instance) -> Vec<Vec<usize>> {
     }
 
     sets
-}
-
-fn possible_landing_times(
-    arrival: &Arrival,
-    arrival_idx: usize,
-    prev_sched: Option<&Schedule>,
-    instance: &Instance,
-) -> Range<NaiveTime> {
-    match prev_sched {
-        None => arrival.window.earliest()..arrival.window.latest(),
-        Some(prev_sched) => {
-            let sep = instance.separations()[(prev_sched.flight_index(), arrival_idx)];
-            let earliest_landing = arrival
-                .window
-                .earliest()
-                .max(prev_sched.flight_time() + sep);
-
-            earliest_landing..arrival.window.latest()
-        },
-    }
-}
-
-fn possible_takeoff_times(
-    departure: &Departure,
-    departure_idx: usize,
-    prev_sched: Option<&Schedule>,
-    instance: &Instance,
-) -> Range<NaiveTime> {
-    match prev_sched {
-        None => departure.ctot.earliest()..departure.ctot.latest(),
-        Some(prev_sched) => {
-            let sep = instance.separations()[(prev_sched.flight_index(), departure_idx)];
-            let earliest_takeoff = departure
-                .ctot
-                .earliest()
-                .max(prev_sched.flight_time() + sep);
-
-            earliest_takeoff..departure.ctot.latest()
-        },
-    }
-}
-
-fn possible_deice_times(
-    departure: &Departure,
-    takeoff: NaiveTime,
-    prev_dep_sched: Option<&DepartureSchedule>,
-    instance: &Instance,
-) -> Range<NaiveTime> {
-    match prev_dep_sched {
-        None => {
-            let latest_deice =
-                takeoff - departure.lineup_dur - departure.taxi_out_dur - departure.deice_dur;
-            let earliest_deice = latest_deice - instance.max_slack_dur;
-
-            earliest_deice..latest_deice
-        },
-        Some(prev_dep_sched) => {
-            let prev_dep = instance.flights()[prev_dep_sched.flight_idx]
-                .as_departure()
-                .unwrap();
-            let prev_deice_finish = prev_dep_sched.deice + prev_dep.deice_dur;
-
-            let latest_deice =
-                takeoff - departure.lineup_dur - departure.taxi_out_dur - departure.deice_dur;
-            let earliest_deice = prev_deice_finish.max(latest_deice - instance.max_slack_dur);
-
-            earliest_deice..latest_deice
-        },
-    }
-}
-
-fn landing_cost(landing: NaiveTime, arrival: &Arrival) -> u64 {
-    let delay = landing - arrival.window.target;
-    delay.num_minutes().unsigned_abs().pow(2)
-}
-
-fn takeoff_cost(takeoff: NaiveTime, departure: &Departure) -> u64 {
-    let delay = takeoff - departure.ctot.target;
-    delay.num_minutes().unsigned_abs().pow(2)
-}
-
-fn slack_cost(deice: NaiveTime, takeoff: NaiveTime, departure: &Departure) -> u64 {
-    let tightest_deice =
-        takeoff - departure.lineup_dur - departure.taxi_out_dur - departure.deice_dur;
-    let slack = tightest_deice - deice;
-    slack.num_minutes().unsigned_abs().pow(2)
 }
