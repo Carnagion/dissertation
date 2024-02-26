af9126d80270b79095fdbb153351dd18ccffe407
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-02-26 13:44:00 +0000

    Update main library root

--- irdis/src/lib.rs
+++ irdis/src/lib.rs
@@ -1,1 +1,15 @@
+pub mod instance {
+    #[doc(inline)]
+    pub use irdis_instance::{flight, schedule, sep, time, Instance};
+}
 
+pub mod solve {
+    pub use irdis_instance::Solve;
+
+    #[cfg(feature = "branch-bound")]
+    pub use irdis_branch_bound::BranchBound;
+}
+
+#[cfg(feature = "vis")]
+#[doc(inline)]
+pub use irdis_vis as vis;
