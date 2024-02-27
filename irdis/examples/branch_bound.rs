a58977d32a1cf1f2958fcf79d4feca6b1fc218f1
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-02-27 10:00:18 +0000

    Update branch-and-bound example

--- irdis/examples/branch_bound.rs
+++ irdis/examples/branch_bound.rs
@@ -1,10 +1,17 @@
+use std::num::NonZeroUsize;
+
 use irdis::{instance::Instance, solve::BranchBound, vis::Visualiser};
+use irdis_instance::Solve;
 
 fn main() {
-    let toml = include_str!("../instances/furini/limited/1.toml");
+    let toml = include_str!("../instances/furini/converted/1.toml");
     let instance = toml::from_str::<Instance>(toml).unwrap();
-    let schedule = instance.solve::<BranchBound>();
+
+    let branch_bound = BranchBound::with_rolling_horizon(NonZeroUsize::new(12).unwrap());
+    let schedule = branch_bound.solve(&instance).unwrap();
+    println!("{:?}", schedule);
+
     let vis = Visualiser::new();
     let doc = vis.visualise(&schedule, &instance).unwrap();
-    svg::save("visuals/furini/limited/1.svg", &doc).unwrap();
+    svg::save("visuals/furini/converted/1.svg", &doc).unwrap();
 }
