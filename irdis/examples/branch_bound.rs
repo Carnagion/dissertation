a9b930c9a059bc818e0fb02bc5961020fad0332d
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-02-27 19:48:10 +0000

    Update example

--- irdis/examples/branch_bound.rs
+++ irdis/examples/branch_bound.rs
@@ -7,7 +7,9 @@ fn main() {
     let toml = include_str!("../instances/furini/converted/1.toml");
     let instance = toml::from_str::<Instance>(toml).unwrap();
 
-    let branch_bound = BranchBound::with_rolling_horizon(NonZeroUsize::new(12).unwrap());
+    // NOTE: Using a large horizon produces better results especially when using
+    //       `earliest()` instead of `target` as the objective (see `src/cost.rs`).
+    let branch_bound = BranchBound::with_rolling_horizon(NonZeroUsize::new(15).unwrap());
     let schedule = branch_bound.solve(&instance).unwrap();
     println!("{:?}", schedule);
 
