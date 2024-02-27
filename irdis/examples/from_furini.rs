5e2c7a70dd3974426f44e41785dc8d7c6ec600a7
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-02-27 19:42:54 +0000

    Update time windows for arrivals

--- irdis/examples/from_furini.rs
+++ irdis/examples/from_furini.rs
@@ -63,21 +63,24 @@ fn instance_from_furini(flights: &str, separations: &str, limit: usize) -> Insta
             let kind = parts.next();
 
             let target = NaiveTime::parse_from_str(parts.next().unwrap(), "%H%M").unwrap();
-            let window = TimeWindow {
-                before: minute * 5,
-                target,
-                after: minute * 10,
-            };
 
             let _unknown = parts.next();
 
             let flight = match kind {
                 Some("A") => Flight::Arr(Arrival {
-                    window,
+                    window: TimeWindow {
+                        before: minute * 5,
+                        target,
+                        after: minute * 5,
+                    },
                     taxi_in_dur: minute * 5,
                 }),
                 Some("D") => Flight::Dep(Departure {
-                    ctot: window,
+                    ctot: TimeWindow {
+                        before: minute * 5,
+                        target,
+                        after: minute * 10,
+                    },
                     pushback_dur: minute * 5,
                     taxi_deice_dur: minute * 5,
                     deice_dur: minute * 5,
