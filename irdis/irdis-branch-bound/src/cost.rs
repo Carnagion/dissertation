a9f2a43d2149d8af41f50135de3514e570f3e597
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-02-29 11:52:28 +0000

    Adjust cost functions based on new optimisations

--- irdis/irdis-branch-bound/src/cost.rs
+++ irdis/irdis-branch-bound/src/cost.rs
@@ -3,9 +3,6 @@ use irdis_instance::{
     schedule::{ArrivalSchedule, DepartureSchedule},
 };
 
-// NOTE: Increasing the cost for violations has seemingly has no effect when
-//       the objective function is set to using `earliest()` instead of `target`
-//       (see below).
 const VIOLATION_COST: u64 = 60;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
@@ -30,19 +27,12 @@ pub fn arrival_cost(sched: &ArrivalSchedule, arr: &Arrival) -> u64 {
         VIOLATION_COST.pow(2)
     };
 
-    // NOTE: Using `earliest()` instead of `target` makes solutions worse.
-    let deviation = (sched.landing - arr.window.target)
+    let deviation = (sched.landing - arr.window.earliest())
         .num_minutes()
         .unsigned_abs()
         .pow(2);
 
-    // NOTE: Using the flight index does NOT fix the worsening of solutions from
-    //       using `earliest()` instead of `target` (see below), but does make them
-    //       slightly better. Maybe breaks symmetries?
-    //
-    //       Using the flight index seems to lead to better solutions even when using
-    //       `target` instead of `earliest()`, allowing the use of smaller horizons.
-    violation + deviation + sched.flight_idx as u64
+    violation + deviation
 }
 
 pub fn departure_cost(sched: &DepartureSchedule, dep: &Departure) -> u64 {
@@ -52,8 +42,6 @@ pub fn departure_cost(sched: &DepartureSchedule, dep: &Departure) -> u64 {
         VIOLATION_COST.pow(2)
     };
 
-    // NOTE: Unlike above, using `earliest()` instead of `target` here does NOT make solutions
-    //       worse.
     let deviation = (sched.takeoff - dep.ctot.earliest())
         .num_minutes()
         .unsigned_abs()
@@ -61,9 +49,8 @@ pub fn departure_cost(sched: &DepartureSchedule, dep: &Departure) -> u64 {
 
     let slack = (sched.takeoff - dep.lineup_dur - dep.taxi_out_dur - dep.deice_dur - sched.deice)
         .num_minutes()
-        .unsigned_abs();
+        .unsigned_abs()
+        .pow(2);
 
-    // NOTE: NOT using the flight index seems to lead to better solutions, regardless of
-    //       whether we are using `target` or `earliest()` (see above).
     violation + deviation + slack
 }
