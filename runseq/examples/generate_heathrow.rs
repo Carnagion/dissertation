297f999080555d81a1d9248bc150a45097fb5819
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-04-06 21:26:17 +0200

    Update Heathrow dataset generation

--- runseq/examples/generate_heathrow.rs
+++ runseq/examples/generate_heathrow.rs
@@ -5,7 +5,10 @@ use std::{
 };
 
 use runseq::{
-    data::{heathrow::from_heathrow, xlsx::to_xlsx},
+    data::{
+        heathrow::{from_heathrow, from_heathrow_with_limits},
+        xlsx::to_xlsx,
+    },
     instance::{flight::Flight, Instance},
 };
 
@@ -18,33 +21,72 @@ fn main() {
     let separation_configs =
         fs::read_to_string("../instances/heathrow/original/runway-separations.csv").unwrap();
 
-    let instances = from_heathrow(&flights, &pushback_durs, &separation_configs).unwrap();
+    let small_instances = from_heathrow_with_limits(
+        &flights,
+        &pushback_durs,
+        &separation_configs,
+        usize::MAX,
+        10,
+    )
+    .unwrap();
 
-    let flight_counts = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 25, 30, 40, 50,
-    ];
-    for (id, flight_count) in (1..).zip(flight_counts) {
-        let instance = instances
+    for (id, deice_count) in (1..=5).zip([0, 1, 2, 3, 4]) {
+        let instance = small_instances
             .iter()
-            .filter(|instance| instance.flights().len() == flight_count)
-            .max_by_key(|instance| deice_count(instance.flights()))
+            .find(|instance| {
+                flights_to_deice(instance.flights()) == deice_count
+                    && instance.flights().len() == 10
+            })
             .unwrap();
 
         save_toml(instance, format!("../instances/heathrow/toml/{}.toml", id));
         save_xlsx(instance, format!("../instances/heathrow/xlsx/{}.xlsx", id));
     }
 
-    let large_instances = instances
+    let upper_small_instances = small_instances
         .iter()
-        .filter(|instance| instance.flights().len() == 55)
-        .take(10);
-    for (id, instance) in (21..).zip(large_instances) {
+        .filter(|instance| {
+            flights_to_deice(instance.flights()) == 5 && instance.flights().len() == 10
+        })
+        .take(5);
+    for (id, instance) in (6..=10).zip(upper_small_instances) {
+        save_toml(instance, format!("../instances/heathrow/toml/{}.toml", id));
+        save_xlsx(instance, format!("../instances/heathrow/xlsx/{}.xlsx", id));
+    }
+
+    let medium_large_instances =
+        from_heathrow(&flights, &pushback_durs, &separation_configs).unwrap();
+
+    for (id, size) in (11..).step_by(2).zip([15, 25, 35, 45, 55]) {
+        let mut instances = medium_large_instances
+            .iter()
+            .filter(|instance| instance.flights().len() == size);
+
+        let instance = instances.next().unwrap();
+        save_toml(instance, format!("../instances/heathrow/toml/{}.toml", id));
+        save_xlsx(instance, format!("../instances/heathrow/xlsx/{}.xlsx", id));
+
+        let instance = instances.next().unwrap();
+        save_toml(
+            instance,
+            format!("../instances/heathrow/toml/{}.toml", id + 1),
+        );
+        save_xlsx(
+            instance,
+            format!("../instances/heathrow/xlsx/{}.xlsx", id + 1),
+        );
+    }
+
+    let large_instances = medium_large_instances
+        .iter()
+        .filter(|instance| instance.flights().len() == 60);
+    for (id, instance) in (21..=30).zip(large_instances) {
         save_toml(instance, format!("../instances/heathrow/toml/{}.toml", id));
         save_xlsx(instance, format!("../instances/heathrow/xlsx/{}.xlsx", id));
     }
 }
 
-fn deice_count(flights: &[Flight]) -> usize {
+fn flights_to_deice(flights: &[Flight]) -> usize {
     flights
         .iter()
         .filter_map(|flight| flight.as_departure()?.deice.as_ref())
