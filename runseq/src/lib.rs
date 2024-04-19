b9f0276d7401e3ccae1a7ae6c6aa3949516e1307
Carnagion <indraneel.mahendrakumar@gmail.com>
2024-04-19 13:31:57 +0200

    Add lib comment

--- runseq/src/lib.rs
+++ runseq/src/lib.rs
@@ -1,4 +1,7 @@
+//! Integrated runway sequencing and de-icing framework, including core types and definitions, solvers, data converters, and a visualiser.
+
 #![deny(rust_2018_idioms)]
+#![warn(missing_docs)]
 
 #[doc(inline)]
 pub use runseq_instance as instance;
